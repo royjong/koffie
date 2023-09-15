b13b265aa88cc74676fd18b31de7bacf63ff6f3f
Roy <rjong1033@gmaill.com>
2023-09-15 21:15:14 +0200

    ready

--- pages/index.js
+++ pages/index.js
@@ -1,92 +1,108 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
 
-export default function Home() {
-  const [emailContent, setEmailContent] = useState('');
-  const [subscribedEmails, setSubscribedEmails] = useState([]);
+export default function LandingPage() {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-  const fetchEmail = async () => {
-    try {
-      const res = await axios.get('http://127.0.0.1:5000/generate-email');
-      setEmailContent(res.data.generated_email);
-    } catch (error) {
-      console.error('There was an error fetching the email', error);
-    }
+  const isValidEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
   };
 
-  const fetchSubscribedEmails = async () => {
-    try {
-      const res = await axios.get('/api/getEmail');
-      setSubscribedEmails(res.data.emails);
-    } catch (error) {
-      console.error('There was an error fetching the subscribed emails', error);
-    }
-  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-  const sendEmailToSubscribers = async () => {
-    try {
-      const res = await axios.post('/api/sendEmails', {
-        emailContent,
+    if (!isValidEmail(email)) {
+      setMessage('Please enter a valid email.');
+      return;
+    }
+
+    try { // Fixed here: Added missing '{'
+      const res = await fetch('/api/saveEmail', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
       });
 
       if (res.status === 200) {
-        console.log('Emails sent successfully');
+        setMessage('Bedankt voor je aanmelding!');
+        setEmail(''); // Clear the email input field
+      } else {
+        setMessage('Er ging iets mis. Probeer het opnieuw.');
       }
     } catch (error) {
-      console.error('There was an error sending the emails', error);
+      setMessage('Er ging iets mis. Probeer het opnieuw.');
     }
   };
 
-  useEffect(() => {
-    fetchSubscribedEmails();
-  }, []);
-
   return (
     <div className="min-h-screen bg-gray-100 font-sans">
-    <nav className="bg-white p-4">
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="text-indigo-600 text-2xl font-bold">Koffie Momentje Dashboard</div>
-        <div className="space-x-4">
-          <button className="text-gray-600" onClick={fetchEmail}>Generate New Email</button>
-          <button className="text-gray-600" onClick={sendEmailToSubscribers}>Send to Email List</button>
-        </div>
-      </div>
-    </nav>
-
-    <section className="container mx-auto p-8">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        {/* Card 1: Email Subscribers */}
-        <div className="rounded bg-white p-4 shadow-lg">
-          <h2 className="text-2xl font-semibold text-gray-900 mb-4">Latest Subscribed Emails</h2>
-          <div className="h-[200px] overflow-y-auto">
-            <ul>
-              {subscribedEmails.slice(0, 20).map((item, index) => (
-                <li key={index} className="mb-2 text-gray-700">{item.email}</li>
-              ))}
-            </ul>
+      {/* Navbar */}
+      <nav className="bg-white p-4">
+        <div className="container mx-auto flex items-center justify-between">
+          <img className='w-24 md:w-[250px]' src="https://i.ibb.co/C0gGXvt/KOFFIE-removebg-preview.png" />
+          <div className="space-x-4">
+            <a href="#" className="text-gray-600">Home</a>
+            <a href="#" className="text-gray-600">Partner contact</a>
           </div>
         </div>
+      </nav>
 
-        {/* Card 2: Placeholder */}
-        <div className="rounded bg-white p-4 shadow-lg">
-          <h2 className="text-2xl font-semibold text-gray-900 mb-4">Card 2 Placeholder</h2>
-          <p className="text-gray-700">Content for card 2.</p>
+      {/* Hero Section */}
+      <section className="flex flex-col items-center justify-center h-[50vh] lg:h-[70vh] bg-white text-dark px-4">
+        <div className="text-center">
+          <h1 className="text-2xl md:text-2xl lg:text-4xl font-extrabold text-gray-900 mb-2 md:mb-4">
+            Lees elke ochtend het belangrijkste nieuws binnen 1 minuut in je mailbox!☕
+          </h1>
+          <p className="text-sm md:text-md lg:text-lg text-gray-700 mb-2 md:mb-4">
+            Meld je gratis aan, en ontvang elke dag om stipt 08:00 het nieuws in een overzichtelijk formaat in je mail!
+          </p>
+          <form className="space-y-4 w-full max-w-xs md:max-w-md mx-auto" onSubmit={handleSubmit}>
+            <input 
+              type="email" 
+              name="email" 
+              placeholder="youremail@domain.com" 
+              className="w-full p-4 rounded-lg bg-opacity-80"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} 
+            />
+            <button type="submit" className="w-full p-4 rounded-lg bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-4 focus:ring-indigo-300">
+              Yes, send me news!
+            </button>
+            {message && <p className="mt-4 text-indigo-200">{message}</p>}
+          </form>
         </div>
+      </section>
 
-        {/* Card 3: Placeholder */}
-        <div className="rounded bg-white p-4 shadow-lg">
-          <h2 className="text-2xl font-semibold text-gray-900 mb-4">Card 3 Placeholder</h2>
-          <p className="text-gray-700">Content for card 3.</p>
+      {/* Footer */}
+      <footer className="bg-gray-200 text-gray-600 py-8 mt-auto">
+        <div className="container mx-auto text-center">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-8 text-center">
+            <div>
+              <h3 className="font-semibold mb-2">Contact</h3>
+              <a href="#" className="block">Contact Page</a>
+            </div>
+            <div>
+              <h3 className="font-semibold mb-2">Legal</h3>
+              <p>KVK: 12345678</p>
+              <p>BTW: NL123456789B01</p>
+            </div>
+            <div>
+              <h3 className="font-semibold mb-2">Mail</h3>
+              <p>info@example.com</p>
+            </div>
+            <div>
+              <h3 className="font-semibold mb-2">Partners</h3>
+              <a href="#" className="block">Partner Page</a>
+            </div>
+          </div>
+          <div className="mt-4">
+            <p>&copy; 2023 KoffieMomentje. All rights reserved.</p>
+          </div>
         </div>
-      </div>
-
-      {emailContent && (
-        <div className="mt-8 rounded bg-white p-4 shadow-lg">
-          <h2 className="text-2xl font-semibold text-gray-900 mb-4">Generated Email</h2>
-          <p className="text-gray-700">{emailContent}</p>
-        </div>
-      )}
-    </section>
-  </div>
+      </footer>
+    </div>
   );
 }
